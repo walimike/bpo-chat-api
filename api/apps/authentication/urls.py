ff82d1f18763917e82256cbd1fe4630f62eef3e6
walimike <walimike139@gmail.com>
2020-03-10 09:49:13 +0300

    user should login [Delivers #171683505]

--- api/apps/authentication/urls.py
+++ api/apps/authentication/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 from .views import RegistrationAPIView, HelloView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('user/', RegistrationAPIView.as_view()),
     path('hello/', HelloView.as_view()),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
