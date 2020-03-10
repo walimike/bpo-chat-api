4fe15b2f6d238f076213e860d5c05d992eda1489
walimike <walimike139@gmail.com>
2020-03-10 08:18:12 +0300

    modify token authentication to simple jwt

--- api/apps/authentication/models.py
+++ api/apps/authentication/models.py
@@ -1,11 +1,7 @@
-from django.conf import settings
 from django.contrib.auth.models import (
     AbstractBaseUser, BaseUserManager
 )
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
 
 
 class UserManager(BaseUserManager):
@@ -50,9 +46,3 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return self.email
-
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
--- api/apps/authentication/urls.py
+++ api/apps/authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import RegistrationAPIView
+from .views import RegistrationAPIView, HelloView
 
 urlpatterns = [
     path('user/', RegistrationAPIView.as_view()),
+    path('hello/', HelloView.as_view()),
 ]
