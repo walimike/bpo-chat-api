01aba1c0b992297bfea47f8dc96e8b969768ae1b
walimike <walimike139@gmail.com>
2020-03-11 23:13:35 +0300

    - add chat tests - refactor tests

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,53 +1,38 @@
-from django.urls import reverse
-from rest_framework.test import APITestCase
 from rest_framework import status
-from api.apps.authentication.models import User
+
+from .test_base import BaseTest
 
 
-class Authentication(APITestCase):
+class TestAuthentication(BaseTest):
 
     def test_for_new_user(self):
-        data =  {"username": "akram",
-                "email": "akram@gmail.com",
-                "password": "password123",
-                "password2": "password123"}
-
-        response = self.client.post('/api/v1/users/signup/', data, format='json')
+        response = self.client.post('/api/v1/users/signup/', self.valid_registration_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_for_missing_username(self):
-        """
-        Method for testing if there is a missing username during registration.
-        """
-        data = {"username": "", 
-                "email": "akram@gmail.com",
-                "password": "password123",
-                "password2": "password123"}
-        response = self.client.post('/api/v1/users/signup/', data, format='json')
+        response = self.client.post('/api/v1/users/signup/', self.invaid_user_name, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("This field may not be blank.", str(response.data))
 
     def test_for_missing_email(self):
-        """
-        Method for testing if there is a missing email during registration.
-        """
-        data = {"user": {"username": "mukasa", "email": "", "password":
-                "akrammukasa"}}
-        response = self.client.post('/api/v1/users/signup/', data, format='json')
+        response = self.client.post('/api/v1/users/signup/', self.invaid_email, format='json')
+
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn("This field is required", str(response.data))
+        self.assertIn("Enter a valid email address", str(response.data))
+
+    def test_for_non_matching_passwords(self):
+        response = self.client.post('/api/v1/users/signup/', self.non_matching_passwords, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("Passwords must much", str(response.data))
+
+    def test_unique_user_email(self):
+        self.client.post('/api/v1/users/signup/', self.valid_registration_data, format='json')
+        re = self.client.post('/api/v1/users/signup/', self.valid_registration_data, format='json')
+        self.assertEqual(re.status_code, status.HTTP_400_BAD_REQUEST)
+
 
     def test_login(self):
-        data =  {"username": "akram",
-                "email": "akram@gmail.com",
-                "password": "password123",
-                "password2": "password123"}
-
-        response = self.client.post('/api/v1/users/signup/', data, format='json')
-
-        data = {"email": "akram@gmail.com", 
-                "password": "password123"}
-        response = self.client.post('/api/v1/users/login/', data, format='json')
-
+        response = self.register_and_login() 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
