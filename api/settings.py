6f75b5dddb365f8f038da44020093d5f354722db
walimike <walimike139@gmail.com>
2020-03-12 11:09:55 +0300

    debug heroku whitenoise error

--- api/settings.py
+++ api/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
