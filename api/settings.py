fed88e7d0a55514099d41935f7805128b67c365f
walimike <walimike139@gmail.com>
2020-03-13 10:16:19 +0300

    add api documentation

--- api/settings.py
+++ api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'drf_yasg',
 
     'api.apps.authentication',
     'api.apps.chat',
