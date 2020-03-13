25d4c68cb104fb3971fbd155564913a63f6d29ce
walimike <walimike139@gmail.com>
2020-03-13 10:41:01 +0300

    add descriptive documentation

--- api/urls.py
+++ api/urls.py
@@ -23,12 +23,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="BPO-CHAT API",
       default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      description="A chat app for individuals and groups",
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
